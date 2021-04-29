abd588020efafc06a757d3dc78ac4b1b24233c7f
Garrison Hess <garrison.hess@gmail.com>
2021-04-29 15:41:20 -0400

    merged code into src.  added knnw dataset in datasets.py

--- src/train.py
+++ src/train.py
@@ -14,8 +14,24 @@ def train_model(config, **kwargs):
     if cfg['spec_augment']:
         transforms = spec_augment
     
-    train_dataset = ASRDataset(train_path, train_transcripts_path, transforms=transforms)
-    val_dataset = ASRDataset(val_path, val_transcripts_path)
+
+    #####  SET THE KNNW VARIABLES HERE
+    #####  CREATE NEW CONFIG FIELDS FOR PATHS
+    if cfg['dataset'] == "KNNW":
+        train_dataset = KnnwAudioDataset(audio_path=None
+                                        , subtitle_lookup_path=None
+                                        , total_frames=None
+                                        , total_duration=None)
+        val_dataset = KnnwAudioDataset(audio_path=None
+                                    , subtitle_lookup_path=None
+                                    , total_frames=None
+                                    , total_duration=None)
+    else:
+        train_dataset = ASRDataset(train_path, train_transcripts_path, transforms=None)
+        val_dataset = ASRDataset(val_path, val_transcripts_path)
+
+
+
     if cfg['minival']:
         val_dataset = torch.utils.data.Subset(val_dataset, [x for x in range(cfg['threshold'])])
 
